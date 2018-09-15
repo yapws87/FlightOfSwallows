49a9785f106509c680c2af4b43bc045cbdc107c8
yapws87 <yapws87@gmail.com>
2018-09-15 17:31:57 +0000

    added run bash

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+motor_pin=25
+light_pin=24
+input_pin=23
+
+gpio -g mode $motor_pin output
+gpio -g mode $light_pin output
+
+gpio -g write $motor_pin 1
+gpio -g write $light_pin 1
+
+~/fos_project/FlightOfSwallows/./PiCamera 320 240 90
