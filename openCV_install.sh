093574572597582ed4b80afb98befed5133ace60
yapws87 <yapws87@gmail.com>
2018-09-28 22:45:16 +0900

    asd

--- openCV_install.sh
+++ openCV_install.sh
@@ -98,12 +98,12 @@ mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
-    # -D INSTALL_PYTHON_EXAMPLES=ON \
 	-D ENABLE_NEON=ON \
 	-D WITH_LIBV4L=ON \
 	-D ENABLE_PRECOMPILED_HEADERS=OFF \
 	.. /
-    # -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.4.3/modules \
+	# -D INSTALL_PYTHON_EXAMPLES=ON \
+	# -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.4.3/modules \
     # -D BUILD_EXAMPLES=ON ..
 
 make -j4
