e406ab007f8f0ee7e92c21d8e0a845f154847c09
yapws87 <yapws87@gmail.com>
2018-10-01 01:42:40 +0900

    .

--- BirdCounter.cpp
+++ BirdCounter.cpp
@@ -647,6 +647,7 @@ void _printStatus_thread_func(int nTime, int nSaturation, int nOverflow, int nCo
 	std::ofstream status_file;
 	std::string filename = "/home/pi/openCV_test/bird_log/" + picom.get_current_date();
 
+	picom.printStdLog("Opening file for status print",1)
 	status_file.open(filename + "_status.txt", std::fstream::out | std::fstream::app);
 	
 	if(status_file.is_open())
@@ -677,6 +678,7 @@ void _printStatus_thread_func(int nTime, int nSaturation, int nOverflow, int nCo
 			<< "core=" << core_val<< "\t"
 			;
 
+		picom.printStdLog("Print Done",1)
 		status_file.close();
 	}
 	
