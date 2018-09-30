20b6b34c9ed9dc7e74638372de7c109a79ebdf29
yapws87 <yapws87@gmail.com>
2018-10-01 02:18:26 +0900

    update

--- bootStart/bootstart.sh
+++ bootStart/bootstart.sh
@@ -1,4 +1,5 @@
-sudo python /home/pi/Desktop/Twitter/HiWorld.py
+sudo python /home/pi/projects/FlightOfSwallows/Twitter/HiWorld.py
 sudo modprobe bcm2835-v4l2
-sudo /home/pi/openCV_test/./PiCamera
+v4l2-ctl --set-ctrl horizontal_flip=1 vertical_flip=1
+sudo bash /home/pi/projects/FlightOfSwallows/.run.sh
 
