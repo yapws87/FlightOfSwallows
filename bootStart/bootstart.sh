2721f357550667471c1320bfefcfe3a6495918e9
yapws87 <yapws87@gmail.com>
2018-10-01 17:47:22 +0900

    .

--- bootStart/bootstart.sh
+++ bootStart/bootstart.sh
@@ -2,5 +2,5 @@
 #sudo python /home/pi/projects/FlightOfSwallows/twitter/HiWorld.py
 sudo modprobe bcm2835-v4l2
 v4l2-ctl --set-ctrl=rotate=180
-sudo bash /home/pi/projects/FlightOfSwallows/./run.sh
+#sudo bash /home/pi/projects/FlightOfSwallows/./run.sh
 
