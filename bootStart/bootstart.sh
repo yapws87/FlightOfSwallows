87fd31535147135a0ee6d86fd458fd8bf6eb6e8d
yapws87 <yapws87@gmail.com>
2018-10-01 17:36:12 +0900

    test

--- bootStart/bootstart.sh
+++ bootStart/bootstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #sudo python /home/pi/projects/FlightOfSwallows/twitter/HiWorld.py
 sudo modprobe bcm2835-v4l2
-v4l2-ctl --set-ctrl horizontal_flip=1 --set-ctrl vertical_flip=1
+v4l2-ctl --set-ctrl=rotate=180
 sudo bash /home/pi/projects/FlightOfSwallows/./run.sh
 
