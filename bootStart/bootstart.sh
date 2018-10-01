6fb01d67392d7c5bf4cd1498b21ee90ae0b89073
yapws87 <yapws87@gmail.com>
2018-10-01 10:43:52 +0900

    remove tweets

--- bootStart/bootstart.sh
+++ bootStart/bootstart.sh
@@ -1,4 +1,4 @@
-sudo python /home/pi/projects/FlightOfSwallows/twitter/HiWorld.py
+#sudo python /home/pi/projects/FlightOfSwallows/twitter/HiWorld.py
 sudo modprobe bcm2835-v4l2
 v4l2-ctl --set-ctrl horizontal_flip=1 --set-ctrl vertical_flip=1
 sudo bash /home/pi/projects/FlightOfSwallows/.run.sh
