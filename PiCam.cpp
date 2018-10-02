88aa65348ce0426dc126bf337a7383aa5a0eea3e
yapws87 <yapws87@gmail.com>
2018-10-02 12:41:42 +0900

    .

--- PiCam.cpp
+++ PiCam.cpp
@@ -121,7 +121,7 @@ void PiCam::runFrame_thread()
 			}
 			
 			// Que to save image
-			if (picom.get_current_time() == "11:43:00" )
+			if (picom.get_current_time() == "11:45:00" )
 			{
 				if(!m_bRecord){
 					m_bRecord = true;
