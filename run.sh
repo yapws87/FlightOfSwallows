dd677f53fb417d980702e0f05ee04a741b270b10
yapws87 <yapws87@gmail.com>
2018-10-06 23:44:50 +0900

    video setting

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ gpio -g mode $light_pin output
 gpio -g write $motor_pin 1
 gpio -g write $light_pin 1
 
-v4l2-ctl --set-ctrl=rotate=180
+v4l2-ctl --set-ctrl=rotate=180 -c auto_exposure=1 -c exposure_time_absolute=500 -c white_balance_auto_preset=0 -c exposure_metering_mode=1 -c brightness=60 -c saturation=80 -c contrast=15
 
 /home/pi/projects/FlightOfSwallows/./PiCamera 321 240 90 0
