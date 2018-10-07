f21a9a2d5c46ea3ff478b514974aca24edaa2d57
yapws87 <yapws87@gmail.com>
2018-10-07 10:14:18 +0900

    change camera param

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ gpio -g mode $light_pin output
 gpio -g write $motor_pin 1
 gpio -g write $light_pin 1
 
-v4l2-ctl --set-ctrl=rotate=180 -c auto_exposure=1 -c exposure_time_absolute=500 -c white_balance_auto_preset=0 -c exposure_metering_mode=1 -c brightness=60 -c saturation=80 -c contrast=15
+v4l2-ctl --set-ctrl=rotate=180 -c auto_exposure=1 -c exposure_time_absolute=500 -c white_balance_auto_preset=2 -c exposure_metering_mode=1 -c brightness=60 -c saturation=100 -c contrast=15 -c sharpness=100 -c color_effects=1
 
 /home/pi/projects/FlightOfSwallows/./PiCamera 321 240 90 0
