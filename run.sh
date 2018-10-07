664dc34e3b4798b5b35331c9307d3947cb2834a1
yapws87 <yapws87@gmail.com>
2018-10-07 22:20:02 +0900

    as

--- run.sh
+++ run.sh
@@ -12,6 +12,6 @@ gpio -g write $light_pin 1
 
 v4l2-ctl --set-ctrl=rotate=180 -c auto_exposure=1 -c exposure_time_absolute=500 -c white_balance_auto_preset=1 
 v4l2-ctl -c exposure_metering_mode=1 -c brightness=70 -c saturation=100
-v4l2-ctl -c contrast=15 -c sharpness=100 -c color_effects=1 -c scene_mode=11
+v4l2-ctl -c contrast=25 -c sharpness=100 -c color_effects=1 -c scene_mode=11
 
 /home/pi/projects/FlightOfSwallows/./PiCamera 321 240 90 0
