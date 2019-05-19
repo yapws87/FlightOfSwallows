299f1ae377266daf25111bb9798eb27bd161f535
yapws87 <yapws87@gmail.com>
2019-05-19 20:54:32 +0900

    change back to 90 fps

--- run.sh
+++ run.sh
@@ -15,4 +15,4 @@ v4l2-ctl -c iso_sensitivity_auto=1 -c iso_sensitivity=0
 v4l2-ctl -c exposure_metering_mode=1 -c brightness=50 -c saturation=100
 v4l2-ctl -c contrast=25 -c sharpness=100 -c color_effects=1 -c scene_mode=11
 
-/home/pi/projects/FlightOfSwallows/./PiCamera 321 240 60 0
+/home/pi/projects/FlightOfSwallows/./PiCamera 321 240 90 0
