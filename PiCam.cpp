9504070e4900a799e37bd8d710ed95b29d6f0460
yapws87 <yapws87@gmail.com>
2019-10-08 22:01:30 +0900

    change recording time

--- PiCam.cpp
+++ PiCam.cpp
@@ -121,9 +121,9 @@ void PiCam::runFrame_thread()
 			}
 			
 			// Que to save image
-			if ((picom.get_current_time() == "19:12:00" 
+			if ((picom.get_current_time() == "19:00:00" 
 			|| picom.get_current_time() == "07:00:00" 
-			|| picom.get_current_time() == "03:02:00"
+			|| picom.get_current_time() == "15:02:00"
 			) && m_bEnableRecord)
 			{
 				if(!m_bRecording_flag){
