daef83a115a7d32707882ad9ecbfba5241296423
yapws87 <yapws87@users.noreply.github.com>
2019-10-14 10:51:49 +0900

    Update bootstart.sh
    add lan_test function

--- bootStart/bootstart.sh
+++ bootStart/bootstart.sh
@@ -3,4 +3,4 @@ sudo python /home/pi/projects/FlightOfSwallows/twitter/HiWorld.py
 sudo modprobe bcm2835-v4l2
 v4l2-ctl --set-ctrl=rotate=180
 sudo bash /home/pi/projects/FlightOfSwallows/./run.sh
-
+sudo python /home/pi/projects/FlightOfSwallows/python/lan_test.py
